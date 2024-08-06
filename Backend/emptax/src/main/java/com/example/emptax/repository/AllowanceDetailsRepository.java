c079399b9bfd613271489a71231d03963bb8da70
Gowthaam-J <118245781+Gowthaam-J@users.noreply.github.com>
2024-08-06 15:29:52 +0530

    Add files via upload

---
+++ Backend/emptax/src/main/java/com/example/emptax/repository/AllowanceDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.example.emptax.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.emptax.model.AllowanceDetails;
+
+public interface AllowanceDetailsRepository extends JpaRepository<AllowanceDetails, Long> {
+    @Query("select a from AllowanceDetails a where a.employee = :employeeId")
+    List<AllowanceDetails> findByEmployeeId(Long employeeId);
+}
